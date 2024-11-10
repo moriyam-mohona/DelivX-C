dae80815f007a5a8c8e274a5a5ff837351712d08
moriyam-mohona <moriyammohona@gmail.com>
2024-11-10 20:04:20 +0600

    Google API Key Resolved

--- src/Components/Pages/AuthPages/Firebase/firebase.config.jsx
+++ src/Components/Pages/AuthPages/Firebase/firebase.config.jsx
@@ -1,26 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDR5vK0mE3EKRPRF4bI1eeNisjU0bKQs-c",
-//   authDomain: "delivx-e74bf.firebaseapp.com",
-//   projectId: "delivx-e74bf",
-//   storageBucket: "delivx-e74bf.firebasestorage.app",
-//   messagingSenderId: "340739412651",
-//   appId: "1:340739412651:web:95bb558f7c9f61e79d1dee",
-//   measurementId: "G-BRSERX1G41",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
@@ -33,5 +10,4 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_measurementId,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
